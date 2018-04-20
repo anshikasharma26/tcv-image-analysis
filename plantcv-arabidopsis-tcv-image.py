f04bb4c637e0d667b43b73153b435ffeba616aff
Noah Fahlgren <noahfahlgren@gmail.com>
2018-04-20 16:27:20 -0500

    Improved image analysis

--- plantcv-arabidopsis-tcv-image.py
+++ plantcv-arabidopsis-tcv-image.py
@@ -38,10 +38,63 @@ def main():
     img, path, fname = pcv.readimage(filename=args.image, debug=args.debug)
     # Classify healthy and unhealthy plant pixels
     device, masks = pcv.naive_bayes_classifier(img=img, pdf_file=args.pdfs, device=device)
+
+    # Use the identified blue mesh area to build a mask for the pot area
+    # First errode the blue mesh region to remove background
+    device, mesh_errode = pcv.erode(img=masks["Background_Blue"], kernel=9, i=3, device=device, debug=args.debug)
+    # Define a region of interest for blue mesh contours
+    device, pot_roi, pot_hierarchy = pcv.define_roi(img=img, shape='rectangle', device=device, roi=None,
+                                                    roi_input='default', debug=args.debug, adjust=True, x_adj=0,
+                                                    y_adj=500, w_adj=0, h_adj=-650)
+    # Find blue mesh contours
+    device, mesh_objects, mesh_hierarchy = pcv.find_objects(img=img, mask=mesh_errode, device=device, debug=args.debug)
+    # Keep blue mesh contours in the region of interest
+    device, kept_mesh_objs, kept_mesh_hierarchy, kept_mask_mesh, _ = pcv.roi_objects(img=img, roi_type='partial',
+                                                                                     roi_contour=pot_roi,
+                                                                                     roi_hierarchy=pot_hierarchy,
+                                                                                     object_contour=mesh_objects,
+                                                                                     obj_hierarchy=mesh_hierarchy,
+                                                                                     device=device, debug=args.debug)
+    # Flatten the blue mesh contours into a single object
+    device, mesh_flattened, mesh_mask = pcv.object_composition(img=img, contours=kept_mesh_objs,
+                                                               hierarchy=kept_mesh_hierarchy, device=device,
+                                                               debug=args.debug)
+    # Initialize a pot mask
+    pot_mask = np.zeros(np.shape(masks["Background_Blue"]), dtype=np.uint8)
+    # Find the minimum bounding rectangle for the blue mesh region
+    rect = cv2.minAreaRect(mesh_flattened)
+    # Create a contour for the minimum bounding box
+    box = cv2.boxPoints(rect)
+    box = np.int0(box)
+    # Create a mask from the bounding box contour
+    cv2.drawContours(pot_mask, [box], 0, (255), -1)
+    # If the bounding box area is too small then the plant has likely occluded too much of the pot for us to use this
+    # as a marker for the pot area
+    if np.sum(pot_mask) / 255 < 2900000:
+        print(np.sum(pot_mask) / 255)
+        # Create a new pot mask
+        pot_mask = np.zeros(np.shape(masks["Background_Blue"]), dtype=np.uint8)
+        # Set the mask area to the ROI area
+        box = np.array([[0, 500], [0, 2806], [2304, 2806], [2304, 500]])
+        cv2.drawContours(pot_mask, [box], 0, (255), -1)
+    # Dialate the blue mesh area to include the ridge of the pot
+    device, pot_mask_dilated = pcv.dilate(img=pot_mask, kernel=3, i=60, device=device, debug=args.debug)
+    # Mask the healthy mask
+    device, healthy_masked = pcv.apply_mask(img=cv2.merge([masks["Healthy"], masks["Healthy"], masks["Healthy"]]),
+                                            mask=pot_mask_dilated, mask_color="black", device=device, debug=args.debug)
+    # Mask the unhealthy mask
+    device, unhealthy_masked = pcv.apply_mask(img=cv2.merge([masks["Unhealthy"], masks["Unhealthy"],
+                                                             masks["Unhealthy"]]),
+                                              mask=pot_mask_dilated, mask_color="black", device=device,
+                                              debug=args.debug)
+    # Convert the masks back to binary
+    healthy_masked, _, _ = cv2.split(healthy_masked)
+    unhealthy_masked, _, _ = cv2.split(unhealthy_masked)
+
     # Fill small objects
-    device, fill_image_healthy = pcv.fill(img=np.copy(masks["Healthy"]), mask=np.copy(masks["Healthy"]),
+    device, fill_image_healthy = pcv.fill(img=np.copy(healthy_masked), mask=np.copy(healthy_masked),
                                           size=300, device=device, debug=args.debug)
-    device, fill_image_unhealthy = pcv.fill(img=np.copy(masks["Unhealthy"]), mask=np.copy(masks["Unhealthy"]),
+    device, fill_image_unhealthy = pcv.fill(img=np.copy(unhealthy_masked), mask=np.copy(unhealthy_masked),
                                             size=1000, device=device, debug=args.debug)
     # Define a region of interest
     device, roi1, roi_hierarchy = pcv.define_roi(img=img, shape='rectangle', device=device, roi=None,
